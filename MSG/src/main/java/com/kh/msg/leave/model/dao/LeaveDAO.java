d55f65ebcfa71c77ba099abc15abc660638e524f
leeheonjoon <reoka0427@naver.com>
2020-04-08 13:37:50 +0900

    휴가조정 완료

--- MSG/src/main/java/com/kh/msg/leave/model/dao/LeaveDAO.java
+++ MSG/src/main/java/com/kh/msg/leave/model/dao/LeaveDAO.java
@@ -3,12 +3,14 @@ package com.kh.msg.leave.model.dao;
 import java.util.HashMap;
 import java.util.List;
 
+import com.kh.msg.edoc.model.vo.Edoc;
 import com.kh.msg.leave.model.vo.Leave;
 import com.kh.msg.leave.model.vo.LeaveInfoPlus;
 import com.kh.msg.leave.model.vo.LeaveModal;
 import com.kh.msg.leave.model.vo.LeavePlus;
 import com.kh.msg.leave.model.vo.LeaveSet;
 import com.kh.msg.leave.model.vo.MyLeave;
+import com.kh.msg.leave.model.vo.leavePagingVo;
 import com.kh.msg.member.model.vo.Member;
 
 public interface LeaveDAO {
@@ -26,5 +28,13 @@ public interface LeaveDAO {
 	/* List<LeaveModal> selectModalList(); */
 
 	List<LeaveModal> selectModalList(int empNo);
+
+	List<Edoc> selectModalSearch();
+
+	int insertModal(int vctnNo, String edocId, String vctnCd, int vctnAmt, String vctnReason);
+
+	int countBoard();
+
+	List<LeaveModal> selectBoard(leavePagingVo vo);
 
 }
